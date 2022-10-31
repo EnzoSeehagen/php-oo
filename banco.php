dfc89e1dd007e176ed9d37523f390b3739568dce
EnzoSeehagen <enzovonseehagen@gmail.com>
2022-10-30 23:03:11 -0300

    Initial commit

---
+++ banco.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once 'src/Conta.php';
+require_once 'src/Endereco.php';
+require_once 'src/Titular.php';
+require_once 'src/CPF.php';
+
+$endereco = new Endereco('Londrina', 'Aurora', 'Av Jose Gabriel de Oliveira', '915');
+$enzo = new Titular(new CPF('123.456.789-10'), 'Enzo Seehagen', $endereco);
+$primeiraConta = new Conta($enzo);
+$primeiraConta->deposita(500);
+$primeiraConta->saca(300);
+
+echo $primeiraConta->recuperaNomeTitular() . PHP_EOL;
+echo $primeiraConta->recuperaCpfTitular() . PHP_EOL;
+echo $primeiraConta->recuperaSaldo() . PHP_EOL;
+
+$patricia = new Titular(new CPF('698.549.548-10'), 'Patricia', $endereco);
+$segundaConta = new Conta($patricia);
+var_dump($segundaConta);
+
+$outra = new Conta(new Titular(new CPF('123.654.789-01'), 'Abcdefg', $endereco));
+unset($segundaConta);
+echo Conta::recuperaNumeroDeContas();
