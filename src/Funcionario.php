7f6e6beae3ca1e47079e260dd4c0493d77e913ca
EnzoSeehagen <enzovonseehagen@gmail.com>
2022-10-30 23:25:03 -0300

    Criação da classe 'Pessoa' e aplicação de Herança

--- src/Funcionario.php
+++ src/Funcionario.php
@@ -1,29 +1,18 @@
 <?php
 
-class Funcionario
+class Funcionario extends Pessoa
 {
-    private $nome;
-    private $cpf;
     private $cargo;
 
-    public function __construct(string $nome, string $cpf, string $cargo)
+    public function __construct(string $nome, CPF $cpf, string $cargo)
     {
+        $this->validaNomeTitular($nome);
         $this->nome = $nome;
         $this->cpf = $cpf;
         $this->cargo = $cargo;
     }
 
-    public function getNome(): string
-    {
-        return $this->nome;
-    }
-
-    public function getCpf(): string
-    {
-        return $this->cpf;
-    }
-
-    public function getCargo(): string
+    public function recuperaCargo(): string
     {
         return $this->cargo;
     }
